024284b181b42786eb50ce66adb199c4cd287095
maiquocthinh <majquocthjnh@gmail.com>
2022-07-25 13:23:12 +0700

    updated

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
-const handlebars = require('express-handlebars');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cookieParser = require("cookie-parser");
 
 
