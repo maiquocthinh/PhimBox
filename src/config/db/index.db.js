5d9806be898ed81ca45702d4fdb37cdd7ffea286
maiquocthinh <majquocthjnh@gmail.com>
2022-07-25 13:50:51 +0700

    updated

--- src/config/db/index.db.js
+++ src/config/db/index.db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect(){
     try{
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mqt:08520852@cluster0.vd7nr.mongodb.net/phimbox?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mqt:08520852@cluster0.vd7nr.mongodb.net/phimbox');
         console.log('Connect Success');
     }
     catch(error){
