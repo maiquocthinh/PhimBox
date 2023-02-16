82164c5f509473a08064f961349609873c687609
maiquocthinh <majquocthjnh@gmail.com>
2023-02-16 18:38:45 +0700

    update dashboard page

--- src/controllers/admin/dashboard.controller.js
+++ src/controllers/admin/dashboard.controller.js
@@ -4,23 +4,43 @@ const Episodes = require('../../models/episode.models');
 
 // [GET] admin/dashboard
 const dashboard = (req, res) => {
-	const { user } = req.session;
-
 	Promise.all([
 		Users.countDocuments({}),
 		Films.countDocuments({}),
 		Episodes.countDocuments({}),
-		Films.find(
-			{},
+		Films.aggregate([
 			{
-				film_id: 1,
-				film_img: 1,
-				film_originalname: 1,
-				film_year: 1,
-				film_uploadby: 1,
-				updatedAt: 1,
+				$addFields: {
+					createdBy: {
+						$toObjectId: '$createdBy',
+					},
+				},
 			},
-		)
+			{
+				$lookup: {
+					from: 'users',
+					localField: 'createdBy',
+					foreignField: '_id',
+					as: 'createdBy',
+				},
+			},
+			{
+				$project: {
+					id: 1,
+					poster: 1,
+					originalName: 1,
+					type: 1,
+					status: 1,
+					year: 1,
+					uploadBy: 1,
+					createdAt: 1,
+					viewed: 1,
+					viewable: 1,
+					createdBy: { name: '$createdBy.name', avatar: '$createdBy.avatar' },
+				},
+			},
+			// { $unwind: '$createdBy' },
+		])
 			.sort({ _id: -1 })
 			.limit(10),
 		Users.find({}, { user_pass: 0 }).sort({ _id: -1 }).limit(10),
@@ -32,16 +52,17 @@ const dashboard = (req, res) => {
 				episode: totalEpisode,
 			};
 			const films = Films.reduce((films, currentFilm) => {
-				films.push(currentFilm._doc);
+				films.push(currentFilm);
 				return films;
 			}, []);
+
 			const users = Users.reduce((users, currentUser) => {
 				users.push(currentUser._doc);
 				return users;
 			}, []);
 
 			res.render('admin/dashboard', {
-				user,
+				user: req.session.user,
 				totalCount,
 				films,
 				users,
