e4658c44efa13d1463410d74389d541e6f8f037a
maiquocthinh <majquocthjnh@gmail.com>
2023-03-21 00:36:33 +0700

    fix: allow user update our info

--- src/controllers/admin/profile.controller.js
+++ src/controllers/admin/profile.controller.js
@@ -1,13 +1,26 @@
 const roleModels = require('../../models/role.models');
+const userModels = require('../../models/user.models');
+const PERMISSIONS = require('../../config/permission.config');
+const userRoleModels = require('../../models/userRole.models');
+const { getUserLevelHtml } = require('../../utils/ajaxUsers.util');
 
-// [GET] admin/profile
-const profile = async (req, res) => {
+// ###### API ######
+// [PATCH] admin/profile/update/:id
+const update = (req, res) => {
+	const userId = req.session.user._id;
+	const { email, name, password, avatar } = req.body;
+
+	if (userId !== req.params.id) return res.status(500).json({ message: 'You have not permission.' });
+
 	try {
-		const roles = await roleModels.find({}, { _id: 1, name: 1 });
-
-		res.render('admin/profile', {
-			user: req.session.user,
-			roles,
+		userModels.findByIdAndUpdate(userId, { email, name, password, avatar }).then(async () => {
+			// change user info in current session
+			if (req.session.user._id === req.params.id) {
+				const user = await userModels.findById(req.params.id);
+				user.password = undefined;
+				req.session.user = user;
+			}
+			return res.status(200).json({ message: 'Change profile success.' });
 		});
 	} catch (error) {
 		console.log(error);
@@ -15,4 +28,46 @@ const profile = async (req, res) => {
 	}
 };
 
-module.exports = { profile };
+// ###### PAGE ######
+// [GET] admin/profile
+const profile = async (req, res) => {
+	try {
+		const roles = await roleModels.find({}, { _id: 1, name: 1 });
+		const userRole = await userRoleModels.aggregate([
+			{
+				$match: { userId: req.session.user._id },
+			},
+			{
+				$limit: 1,
+			},
+			{
+				$addFields: {
+					roleId: { $toObjectId: '$roleId' },
+				},
+			},
+			{
+				$lookup: {
+					from: 'roles',
+					localField: 'roleId',
+					foreignField: '_id',
+					as: 'role',
+				},
+			},
+			{
+				$unwind: '$role',
+			},
+		]);
+		const levelLabel = getUserLevelHtml(userRole[0].role.permissions);
+
+		res.render('admin/profile', {
+			user: req.session.user,
+			roles,
+			levelLabel,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = { profile, update };
--- src/routes/admin/profile.routes.js
+++ src/routes/admin/profile.routes.js
@@ -3,6 +3,9 @@ const router = Router();
 const authMiddleware = require('../../middlewares/auth.middleware');
 const profileController = require('../../controllers/admin/profile.controller');
 
+// API
+router.patch('/profile/update/:id', authMiddleware.auth, profileController.update);
+
 // PAGE
 router.get('/profile', authMiddleware.auth, profileController.profile);
 
